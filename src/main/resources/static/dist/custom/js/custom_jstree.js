6d72b9c864651cb462c1012fe5e83223c5a15ba6
coldrain-f <intmain__@naver.com>
2022-11-22 18:43:22 +0900

    feat: JsTree View AJAX로 DB에서 데이터 받아와서 렌더링 (#3)

--- src/main/resources/static/dist/custom/js/custom_jstree.js
+++ src/main/resources/static/dist/custom/js/custom_jstree.js
@@ -1,93 +1,29 @@
-$("#jstree").jstree({
-    "core": {
-        "themes": {
-            "responsive": false
-        },
-        "check_callback": true,
-        "data": [
-            {
-                "text": "기초 단어",
-                "icon": "fa fa-folder text-info",
-                "children": [
-                    {
-                        "text": "단어가 읽기다 기본편",
-                        "icon": "fa fa-folder text-primary",
-                        "children": [
-                            {
-                                "text": "Unit 01 ~ Unit 10",
-                                "icon": "fa fa-folder text-danger",
-                                "children": [
-                                    {"text": "Unit 01 - 요리", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 02 - 일상1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 03 - 일상2", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 04 - 학교1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 05 - 학교2", "icon": "fa fa-file text-success"}
-                                ]
-                            },
-                            {"text": "Unit 11 ~ Unit 20", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 21 ~ Unit 30", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 31 ~ Unit 40", "icon": "fa fa-folder text-danger"}
-
-                        ]
-                    },
-                    {
-                        "text": "단어가 읽기다 실력편",
-                        "icon": "fa fa-folder text-primary",
-                        "children": [
-                            {
-                                "text": "Unit 01 ~ Unit 10",
-                                "icon": "fa fa-folder text-danger",
-                                "children": [
-                                    {"text": "Unit 01 - 요리", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 02 - 일상1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 03 - 일상2", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 04 - 학교1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 05 - 학교2", "icon": "fa fa-file text-success"}
-
-                                ]
-                            },
-                            {"text": "Unit 11 ~ Unit 20", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 21 ~ Unit 30", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 31 ~ Unit 40", "icon": "fa fa-folder text-danger"}
-
-                        ]
-                    },
-                    {
-                        "text": "단어가 읽기다 고등편",
-                        "icon": "fa fa-folder text-primary",
-                        "children": [
-                            {
-                                "text": "Unit 01 ~ Unit 10",
-                                "icon": "fa fa-folder text-danger",
-                                "children": [
-                                    {"text": "Unit 01 - 요리", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 02 - 일상1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 03 - 일상2", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 04 - 학교1", "icon": "fa fa-file text-success"},
-                                    {"text": "Unit 05 - 학교2", "icon": "fa fa-file text-success"}
-
-                                ]
-                            },
-                            {"text": "Unit 11 ~ Unit 20", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 21 ~ Unit 30", "icon": "fa fa-folder text-danger"},
-                            {"text": "Unit 31 ~ Unit 40", "icon": "fa fa-folder text-danger"}
-
-                        ]
-                    }
-                ]
+const initJsTreeView = (data) => {
+    console.log("jsTreeData", data);
+    if (!data.length) {
+        alert('JsTree 플러그인 로딩에 실패했습니다.');
+        return false;
+    }
+    $("#jstree").jstree({
+        "core": {
+            "themes": {
+                "responsive": false
             },
-            {
-                "text": "토익 900점",
-                "icon": "fa fa-folder text-info",
-                "children": [
-                    {
-                        "text": "토익 기출문제 단어 200선",
-                        "icon": "fa fa-folder text-primary",
-                        "children": []
-                    }
-                ]
-            }
-        ]
+            "check_callback": true,
+            "data": data,
+        },
+        "plugins": ["dnd", "contextmenu", "state", "types"]
+    });
+};
+
+$.ajax({
+    type: "get",
+    url: "http://localhost:8080/js_tree_items",
+    data: {}, // 전송 파라미터
+    success: (response) => {
+        initJsTreeView(response);
     },
-    "plugins": ["dnd", "contextmenu", "state", "types"]
-});
+    error: (err) => {
+        console.error(err);
+    }
+})
